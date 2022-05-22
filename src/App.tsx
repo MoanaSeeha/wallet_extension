8ff8ed69f066c6f3f14e9e6adcf3cd48307b6168
Prometheus <96538997+MoanaSeeha@users.noreply.github.com>
2022-05-22 17:55:51 +0800

    fffff

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,7 @@
 import React from 'react';
 import './App.css';
-import { DOMMessage, DOMMessageResponse } from './types';
 
 function App() {
-  const [title, setTitle] = React.useState('');
-  const [headlines, setHeadlines] = React.useState<string[]>([]);
-
-  React.useEffect(() => {
-    /**
-     * We can't use "chrome.runtime.sendMessage" for sending messages from React.
-     * For sending messages from React we need to specify which tab to send it to.
-     */
-    chrome.tabs && chrome.tabs.query({
-      active: true,
-      currentWindow: true
-    }, tabs => {
-      /**
-       * Sends a single message to the content script(s) in the specified tab,
-       * with an optional callback to run when a response is sent back.
-       *
-       * The runtime.onMessage event is fired in each content script running
-       * in the specified tab for the current extension.
-       */
-      chrome.tabs.sendMessage(
-        tabs[0].id || 0,
-        { type: 'GET_DOM' } as DOMMessage,
-        (response: DOMMessageResponse) => {
-          setTitle(response.title);
-          setHeadlines(response.headlines);
-        });
-    });
-  });
 
   return (
     <div className="App">
