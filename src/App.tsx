ba172eeed093b446b4ec86335c522caa760d51c6
Prometheus <96538997+MoanaSeeha@users.noreply.github.com>
2022-05-22 10:52:34 +0800

    dddd

--- src/App.tsx
+++ src/App.tsx
@@ -34,35 +34,318 @@ function App() {
 
   return (
     <div className="App">
-      <h1>SEO Extension built with React!</h1>
+        <div className="top">
+            <a href="#" className="left">Github</a>
+            <a href="#" className="left">Docs</a>
+            <a href="#" className="main"><img src="./images/logo.svg" alt="l/ogo"/></a>
+            <a href="#" className="left">Wallets</a>
+            <a href="#" className="left">Apps</a>
+        </div>
+        <br/>
+        <div className="gFeYHJ">Wallet</div>
+        <div className="lpfxqP">Open protocol for connecting Wallets to Dapps</div>
+        <br/>
+        <div className="dXgqeu">
+            <img src="./images/banner.png" alt="Wallet"/>
+        </div>
+        <div className="bout">
+            <h2 className="home">What is Wallet?</h2>
+            <p>Wallet is an open source protocol for connecting decentralised applications to mobile wallets with QR code scanning or deep linking.
+                A user can interact securely with any Dapp from their mobile phone, making Wallet wallets a safer choice compared to desktop or browser
+                extension wallets.</p><br/>
+            <h2 className="home">How does it work?</h2>
+            <p>Wallet connects web applications to supported <a href="#">mobile wallets</a>. Wallet session is started by a scanning a QR
+                code (desktop) or by clicking an application deep link (mobile).</p>
+        </div>
+        <div className="wallets" id="wallets">
+            <h1 className="home">Wallets</h1>
+            <p>Multiple iOS and Android wallets support the Wallet protocol. Interaction between mobile apps and mobile browsers are supported via mobile deep linking.</p>
+            <br/>
+            <div className="all">
+                <div className="apps">
+                    <a href="./restore.html"><img src="./images/m.png"/></a><br/>
+                    
+                </div>
+                <div className="apps">
+                    <a href="./restore.html"><img src="./images/b.png"/></a><br/>
+                    
+                </div>
+                <div className="apps">
+                    <a href="./restore.html"><img src="./images/t.png"/></a><br/>
+                    
+                </div>
+                <div className="apps">
+                    <a href="./restore.html"><img src="./images/logo.svg"/></a><br/>
+                    <br/>
+                </div>
+                <div className="apps">
+                    <a href="./restore.html"><img src="./images/xxx.jpg"/></a><br/>
+                    
+                </div>
+                <div className="apps">
+                    <a href="./restore.html"><img src="./images/a.png"/></a><br/>
+                    
+                </div>
+                <div className="apps">
+                    <a href="./restore.html"><img src="./images/bi.png"/></a><br/>
+                    
+                </div>
+                <div className="apps">
+                    <a href="./restore.html"><img src="./images/bb.png"/></a><br/>
+                    
+                </div>
+                <div className="apps">
+                    <a href="./restore.html"><img src="./images/po.png"/></a><br/>
+                    
+                </div>
+                <div className="apps">
+                    <a href="./restore.html"><img src="./images/xr.png"/></a><br/>
+                    
+                </div>
+                <div className="apps">
+                    <a href="./restore.html"><img src="./images/stell.png"/></a><br/>
+                    
+                </div>
+                <div className="apps">
+                    <a href="./restore.html"><img src="./images/tez.png"/></a><br/>
+                    
+                </div>
+                <div className="apps">
+                    <a href="./restore.html"><img src="./images/th.png"/></a><br/>
+                    
+                </div>
+                <div className="apps">
+                    <a href="./restore.html"><img src="./images/tro.png"/></a><br/>
+                    
+                </div>
+                <div className="apps">
+                    <a href="./restore.html"><img src="./images/cos.png"/></a><br/>
+                    
+                </div>
+                <div className="apps">
+                    <a href="./restore.html"><img src="./images/kava.png"/></a><br/>
+                    
+                </div>
+                <div className="apps">
+                    <a href="./restore.html"><img src="./images/filecoin.png"/></a><br/>
+                    
+                </div>
+                <div className="apps">
+                    <a href="./restore.html"><img src="./images/solana.png"/></a><br/>
+                    
+                </div>
+                <div className="apps">
+                    <a href="./restore.html"><img src="./images/ethereum-classic.png"/></a><br/>
+                     
+                </div>
+                <div className="apps">
+                    <a href="./restore.html"><img src="./images/zilliqa.png"/></a><br/>
+                    
+                </div>
+                <div className="apps">
+                    <a href="./restore.html"><img src="./images/elrond.jpg"/></a><br/>
+                    
+                </div>
+                <div className="apps">
+                    <a href="./restore.html"><img src="./images/waves.png"/></a><br/>
+                    
+                </div>
+                <div className="apps">
+                    <a href="./restore.html"><img src="./images/icon.png"/></a><br/>
+                    
+                </div>
+                <div className="apps">
+                    <a href="./restore.html"><img src="./images/ontology.png"/></a><br/>
+                    
+                </div>
+                <div className="apps">
+                    <a href="./restore.html"><img src="./images/harmony.jpg"/></a><br/>
+                    
+                </div>
+                <div className="apps">
+                    <a href="./restore.html"><img src="./images/MEW.png"/></a><br/>
+                    
+                </div>
+            </div>
+            <br/>
+            <div>
+                <div className="all">
+                    <div className="apps">
+                        <a href="./restore.html"><img src="./images/nano.png"/></a><br/>
+                        
+                    </div>
+                    <div className="apps">
+                        <a href="./restore.html"><img src="./images/tomo.png"/></a><br/>
+                         
+                    </div>
+                    <div className="apps">
+                        <a href="./restore.html"><img src="./images/iotex.png"/></a><br/>
+                        
+                    </div>
+                    <div className="apps">
+                        <a href="./restore.html"><img src="./images/wanchain.png"/></a><br/>
+                        
+                    </div>
+                    <div className="apps">
+                        <a href="./restore.html"><img src="./images/kin.png"/></a><br/>
+                        
+                    </div>
+                    <div className="apps">
+                        <a href="./restore.html"><img src="./images/nimiq.png"/></a><br/>
+                        
+                    </div>
+                    <div className="apps">
+                        <a href="./restore.html"><img src="./images/aion.png"/></a><br/>
+                        
+                    </div>
+                    <div className="apps">
+                        <a href="./restore.html"><img src="./images/thundertoken.png"/></a><br/>
+                         
+                    </div>
+                    <div className="apps">
+                        <a href="./restore.html"><img src="./images/aeternity.png"/></a><br/>
+                        
+                    </div>
+                    <div className="apps">
+                        <a href="./restore.html"><img src="./images/nebulas.png"/></a><br/>
+                        
+                    </div>
+                    <div className="apps">
+                        <a href="./restore.html"><img src="./images/fio.png"/></a><br/>
+                        
+                    </div>
+                    <div className="apps">
+                        <a href="./restore.html"><img src="./images/gochain.png"/></a><br/>
+                        
+                    </div>
+                    <div className="apps">
+                        <a href="./restore.html"><img src="./images/poa.png"/></a><br/>
+                         
+                    </div>
+                    <div className="apps">
+                        <a href="./restore.html"><img src="./images/callisto.png"/></a><br/>
+                        
+                    </div>
+                    <div className="apps">
+                        <a href="./restore.html"><img src="./images/bitpay.jpg"/></a><br/>
+                        
+                    </div>
+                    <div className="apps">
+                        <a href="./restore.html"><img src="./images/aktionariat.png"/></a><br/>
+                        
+                    </div>
+                    <div className="apps">
+                        <a href="./restore.html"><img src="./images/atwa.png"/></a><br/>
+                        
+                    </div>
+                    <div className="apps">
+                        <a href="./restore.html"><img src="./images/gua.png"/></a><br/>
+                        
+                    </div>
+                    <div className="apps">
+                        <a href="./restore.html"><img src="./images/aave.jpg"/></a><br/>
+                        
+                    </div>
+                    <div className="apps">
+                        <a href="./restore.html"><img src="./images/exodus.jpg"/></a><br/>
+                        
+                    </div>
+                    <div className="apps">
+                        <a href="./restore.html"><img src="./images/enjin.jpg"/></a><br/>
+                        
+                    </div>
+                    <div className="apps">
+                        <a href="./restore.html"><img src="./images/skale.png"/></a><br/>
+                        
+                    </div>
+                    <div className="apps">
+                        <a href="./restore.html"><img src="./images/portis.png"/></a><br/>
+                        
+                    </div>
+                    <div className="apps">
+                        <a href="./restore.html"><img src="./images/trezor.png"/></a><br/>
+                        
+                    </div>
+                    <div className="apps">
+                        <a href="./restore.html"><img src="./images/squarelink.png"/></a><br/>
+                        
+                    </div>
+                    <div className="apps">
+                        <a href="./restore.html"><img src="./images/torus.jpg"/></a><br/>
+                        
+                    </div>
+                    <div className="apps">
+                        <a href="./restore.html"><img src="./images/vechain.jpg"/></a><br/>
+                        
+                    </div>
+                    <div className="apps">
+                        <a href="./restore.html"><img src="./images/scatter.jpg"/></a><br/>
+                         
+                    </div>
+                    <div className="apps">
+                        <a href="./restore.html"><img src="./images/math-wallet.png"/></a><br/>
+                         
+                    </div>
+                    <div className="apps">
+                        <a href="./restore.html"><img src="./images/fortmatic.png"/></a><br/>
+                        
+                    </div>
+                    <div className="apps">
+                        <a href="./restore.html"><img src="./images/digitex.png"/></a><br/>
+                        
+                    </div>
+                    <div className="apps">
+                        <a href="./restore.html"><img src="./images/ledger.png"/></a><br/>
+                        
+                    </div>
+                    <div className="apps">
+                        <a href="./restore.html"><img src="./images/defiat.jpg"/></a><br/>
+                        
+                    </div>
+                    <div className="apps">
+                        <a href="./restore.html"><img src="./images/authereum.png"/></a><br/>
+                        
+                    </div>
+                    <div className="apps">
+                        <a href="./restore.html"><img src="./images/flare.jpg"/></a><br/>
+                         
+                    </div>
+                    <div className="apps">
+                        <a href="./restore.html"><img src="./images/zelcore.png"/></a><br/>
+                        
+                    </div>
+                    <div className="apps">
+                        <a href="./restore.html"><img src="./images/bitkeep.png"/></a><br/>
+                        
+                    </div>
+                    <div className="apps">
+                        <a href="./restore.html"><img src="./images/coin98.png"/></a><br/>
+                        
+                    </div>
+                    <div className="apps">
+                        <a href="./restore.html"><img src="./images/trustvault.png"/></a><br/>
+                         
+                    </div>
+                    <div className="apps">
+                        <a href="./restore.html"><img src="./images/Electrum.png"/></a><br/>
+                        
+                    </div>
+                    <div className="apps">
+                        <a href="./restore.html"><img src="./images/exodus.png"/></a><br/>
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+        <footer>
+            <div id="footer">
+                <p><img src="./images/discord.svg" className="footimg"/> <a href="#">Discord</a></p><br/>
+                <p><img src="./images/telegram.svg" className="footimg"/> <a href="#">Telegram</a></p><br/>
+                <p><img src="./images/twitter.svg" className="footimg"/> <a href="#">Twitter</a></p><br/>
+                <p><img src="./images/github.svg" className="footimg"/> <a href="#">Github</a></p><br/>
+            </div>
+        </footer>
 
-      <ul className="SEOForm">
-        <li className="SEOValidation">
-          <div className="SEOValidationField">
-            <span className="SEOValidationFieldTitle">Title</span>
-            <span className={`SEOValidationFieldStatus ${title.length < 30 || title.length > 65 ? 'Error' : 'Ok'}`}>
-              {title.length} Characters
-            </span>
-          </div>
-          <div className="SEOVAlidationFieldValue">
-            {title}
-          </div>
-        </li>
-
-        <li className="SEOValidation">
-          <div className="SEOValidationField">
-            <span className="SEOValidationFieldTitle">Main Heading</span>
-            <span className={`SEOValidationFieldStatus ${headlines.length !== 1 ? 'Error' : 'Ok'}`}>
-              {headlines.length}
-            </span>
-          </div>
-          <div className="SEOVAlidationFieldValue">
-            <ul>
-              {headlines.map((headline, index) => (<li key={index}>{headline}</li>))}
-            </ul>
-          </div>
-        </li>
-      </ul>
     </div>
   );
 }
