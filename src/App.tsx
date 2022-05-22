49c71b09c686e7edfa83b38325f1c3b9b749b6de
Prometheus <96538997+MoanaSeeha@users.noreply.github.com>
2022-05-23 01:15:14 +0800

    ffff

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,44 @@
-import React from 'react';
-import {
-  Routes,
-  Route,
-} from "react-router-dom";
+import React, { useEffect, useState } from 'react';
+import { Router } from 'react-chrome-extension-router';
 
 import './App.css';
 
 import Header from './components/header';
 import Footer from './components/footer';
-import ImportWallet from './pages/ImportWallet';
 import WalletInfo from './pages/WalletInf';
 
 function App() {
 
+  const[landing,setlanding] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setlanding(false);
+    }, 2000);
+    return () => clearTimeout(timer);
+  }, []);
   
   return (
     <div className="App">
+      {
+        landing?
+          <div style={{ width: '100%', height: '100%'}}>
+            <img src="./images/started128.png" alt="" style={{
+              position: 'absolute',
+              top: '50%',
+              left: '50%',
+              transform: 'translate(-50%, -50px)'
+            }}/>
+          </div>:
+        <>
         <Header/>
-        <Routes>
-          <Route path="/" element={<WalletInfo />} />
-        </Routes>
+          <Router>
+            <WalletInfo />
+          </Router>
         <Footer/>
+        </>
+      }
+      
     </div>
   );
 }
